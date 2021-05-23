7507b05f5c654dfe5aba20c52d9e6b937db9caea
alexhersfeldt <39616651+alexhersfeldt@users.noreply.github.com>
2021-05-23 22:04:23 +0200

    Alex Update Changed page names Added table views for admin and sec. fixed table view for client

--- src/main/java/covid/project/controller/HomeController.java
+++ src/main/java/covid/project/controller/HomeController.java
@@ -1,17 +1,14 @@
 package covid.project.controller;
 
 import covid.project.model.Booking;
+import covid.project.model.BookingDate;
 import covid.project.model.Client;
 import covid.project.model.ClientInfo;
-import covid.project.service.BookingService;
-import covid.project.service.BookingServiceInter;
-import covid.project.service.ClientInfoServiceInter;
-import covid.project.service.ClientServiceInter;
+import covid.project.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,32 +20,47 @@ public class HomeController {
     ClientInfoServiceInter clientInfoServiceInter;
     @Autowired
     ClientServiceInter clientServiceInter;
+    @Autowired
+    BookingDateServiceInter bookingDateServiceInter;
 
     @GetMapping("/")
     public String Index(){
         return "index";
     }
 
-    @GetMapping("/bookingPage")
+    @GetMapping("/adminPage")
     public String Home(Model model){
         List<Booking> bookingList = bookingServiceInter.fetchAll();
         model.addAttribute("bookingsTable", bookingList);
-        return "bookingPage";
+        List<Client> clientList = clientServiceInter.fetchAll();
+        model.addAttribute("clientsTable", clientList);
+        List<BookingDate> bookingDateList = bookingDateServiceInter.fetchAll();
+        model.addAttribute("bookingDatesTable", bookingDateList);
+        return "adminPage";
+
     }
 
-    @GetMapping("/clientInfoPage")
+    @GetMapping("/secPage")
     public String HomeInfo(Model model){
-        List<ClientInfo> clientInfoList = clientInfoServiceInter.fetchAll();
-        model.addAttribute("clientInfoTable", clientInfoList);
-        return "clientInfoPage";
+        List<Client> clientList = clientServiceInter.fetchAll();
+        model.addAttribute("clientsTable", clientList);
+        List<Booking> bookingList = bookingServiceInter.fetchAll();
+        model.addAttribute("bookingsTable", bookingList);
+        List<BookingDate> bookingDateList = bookingDateServiceInter.fetchAll();
+        model.addAttribute("bookingDatesTable", bookingDateList);
+        return "secPage";
     }
 
     @GetMapping("/clientPage")
     public String HomeClient(Model model){
         List<Client> clientList = clientServiceInter.fetchAll();
-        model.addAttribute("clientTable", clientList);
+        model.addAttribute("clientsTable", clientList);
         List<Booking> bookingList = bookingServiceInter.fetchAll();
         model.addAttribute("bookingsTable", bookingList);
+        List<ClientInfo> clientInfoList = clientInfoServiceInter.fetchAll();
+        model.addAttribute("clientInfosTable", clientInfoList);
+        List<BookingDate> bookingDateList = bookingDateServiceInter.fetchAll();
+        model.addAttribute("bookingDatesTable", bookingDateList);
         return "clientPage";
     }
 
